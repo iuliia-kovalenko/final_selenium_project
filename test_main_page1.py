487a13921218fdf93dd10b008dd8b3c3a12518ad
iuliia-kovalenko <74493228+iuliia-kovalenko@users.noreply.github.com>
2022-08-06 21:23:11 +0300

    next step passed

---
+++ test_main_page1.py
@@ -0,0 +1,11 @@
+import time
+
+from .pages.main_page import MainPage
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    time.sleep(5)
